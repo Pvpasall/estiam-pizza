f10dc6efa48d8348107e45f91ca2b67e270d911a
el hadji <elhadji.sarr@outlook.com>
2025-06-02 23:37:00 +0200

    feat: added mock data for pizza and ingredients and updated Tables

--- pizza-back/app/main.py
+++ pizza-back/app/main.py
@@ -2,12 +2,15 @@ from fastapi import FastAPI
 from app.database import create_db_and_tables
 from app.routers import pizza
 from app.routers import order
+from app.data import create_test_data
+from app.database import get_session_obj
 
 app = FastAPI(title="Restaurant Pizza API", version="1.0.0")
 
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
+    create_test_data(session=get_session_obj())
 
 app.include_router(pizza.router)
 app.include_router(order.router)
