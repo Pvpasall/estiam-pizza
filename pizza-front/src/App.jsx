b5b27fb69de7fd85d163b672b334acfc6c06c9fb
celaireo <celaireo@gmail.com>
2025-06-04 00:32:32 +0100

    ajout de formulaire de validation de commande

--- pizza-front/src/App.jsx
+++ pizza-front/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import Navbar from './components/Navbar'
-import Home from './pages/Home'
+//import Home from './pages/Home'
 import Menus from './pages/Menus'
 import Contact from './pages/Contact'
 import Paniers from './pages/Paniers'
+import FormulaireCommande from './pages/FormulaireCommande';
 import { useState } from 'react'
 
 
@@ -24,6 +25,7 @@ function App() {
                 <Route path="/" element={<Menus cart={cart} setCart={setCart} />}/>
                 <Route path="/contact" element={<Contact />}/>
                 <Route path="/panier" element={<Paniers cart={cart} handleRemove={handleRemove} />}/>
+                <Route path="/formulaire" element={<FormulaireCommande />} />
             </Routes>
 
 
