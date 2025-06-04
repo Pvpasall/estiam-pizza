5d6c2e6d0a3089836add07ecb7dc9ae2fdc6f753
Arthur277 <arthurdeumenitsako@gmail.com>
2025-06-04 11:25:14 +0200

    css admin

--- pizza-front/src/pages/Login.jsx
+++ pizza-front/src/pages/Login.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 
 import { useAuth } from '../hooks/use-auth';
 import { useNavigate } from 'react-router-dom';
+import '../css/login.css'
+import photo from '../assets/login.png'
 
 
 const LoginPage = () => {
@@ -24,35 +26,43 @@ const LoginPage = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      {error && (
-        <div style={{ color: 'red', marginBottom: '1rem' }}>
-          {error}
-        </div>
-      )}
-      
-      <input
-        type="text"
-        name="username"
-        value={formData.username}
-        onChange={(e) => setFormData({...formData, username: e.target.value})}
-        placeholder="Username"
-        required
-      />
-      
-      <input
-        type="password"
-        name="password"
-        value={formData.password}
-        onChange={(e) => setFormData({...formData, password: e.target.value})}
-        placeholder="Password"
-        required
-      />
-      
-      <button type="submit" disabled={loading}>
-        {loading ? 'Logging in...' : 'Login'}
-      </button>
-    </form>
+
+            <form onSubmit={handleSubmit} className='container'>
+          {error && (
+            <div style={{ color: 'red', marginBottom: '1rem' }}>
+              {error}
+            </div>
+          )}
+          <h1 style={{color:'black'}}>connectez vous ! </h1>
+            <div  className='input-taille2'>
+              <input
+            type="text"
+            name="username"
+            className='input-taille'
+            value={formData.username}
+            onChange={(e) => setFormData({...formData, username: e.target.value})}
+            placeholder="Username"
+            required
+          />
+          
+          <input
+            type="password"
+            name="password"
+            className='input-taille'
+            value={formData.password}
+            onChange={(e) => setFormData({...formData, password: e.target.value})}
+            placeholder="Password"
+            required
+          />
+            </div>
+          <div>
+            <button type="submit" disabled={loading}>
+            {loading ? 'Logging in...' : 'Login'}
+            </button>
+          </div>
+
+        </form>
+
   );
 };
 
